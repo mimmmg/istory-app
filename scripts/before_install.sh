a13f18f1349917c2bbf7d7ebc7e405f4ace146a5
mimmmg <oguma20@gmail.com>
2025-06-28 16:10:58 +0900

    change

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,16 @@
+# #!/bin/bash
+# if [ -d /home/ec2-user/app ]; then
+#     rm -rf /home/ec2-user/app/*
+# else
+#     mkdir -p /home/ec2-user/app
+# fi
 #!/bin/bash
+set -e
+
 if [ -d /home/ec2-user/app ]; then
-    rm -rf /home/ec2-user/app/*
+    echo "🧹 /home/ec2-user/app 디렉토리 정리 중..."
+    sudo rm -rf /home/ec2-user/app/*
 else
+    echo "📂 디렉토리가 없어서 새로 생성합니다"
     mkdir -p /home/ec2-user/app
 fi
